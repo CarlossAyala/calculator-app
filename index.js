69b3c1bd1f5f6856653e18cbaeb9d4381903329a
CarlosAyala <carlosylautaro@gmail.com>
2021-10-09 03:42:03 -0300

    Sección de History añadida

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const btnTheme = document.querySelector('#theme');
+const btnOpenHistory = document.querySelector('#open-h');
+const btnCloseHistory = document.querySelector('#close-h');
+const containerHistory = document.querySelector('.history');
 const containerPhone = document.querySelector('.phone');
 const containerAll = document.querySelector('.container');
 
+console.log(btnCloseHistory)
+
 btnTheme.addEventListener('click', changeTheme);
+btnOpenHistory.addEventListener('click', openHistory);
+btnCloseHistory.addEventListener('click', closeHistory);
 
 function changeTheme(){
   containerPhone.classList.toggle('theme-dark');
@@ -10,9 +17,16 @@ function changeTheme(){
 
   if(containerPhone.classList.contains("theme-dark")){
     this.classList.replace("bx-sun", "bx-moon");
+    this.title = "Theme Dark";
   }else {
     this.classList.replace("bx-moon","bx-sun");
+    this.title = "Theme Light";
   }
 }
 
-console.log(btnTheme)
+function openHistory() {
+  containerHistory.classList.add('open-history');
+}
+function closeHistory() {
+  containerHistory.classList.remove('open-history');
+}
